d6b75f5bd2530252cf28c113b75e8b606db47652
Kanokkorn <kanokorn@outlook.jp>
2019-08-07 07:22:15 +0700

    demo file

---
+++ hello.c
@@ -0,0 +1,15 @@
+#define F_CPU 1000000UL
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <stdint.h>
+
+int main(void){
+    DDRB = 0xff;
+    PORTB = 0x01;
+    while(1){
+	PORTB << 1;
+	delay_ms(100);
+    }
+    
+}
